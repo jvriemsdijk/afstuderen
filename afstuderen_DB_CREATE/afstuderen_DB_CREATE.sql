d424a726ecfe9e41d984a53fdd4e680f5f08efdb
joeyvanriemsdijk <joey1501@gmail.com>
2016-06-11 16:26:37 +0200

    New DB script

--- afstuderen_DB_CREATE/afstuderen_DB_CREATE.sql
+++ afstuderen_DB_CREATE/afstuderen_DB_CREATE.sql
@@ -1,32 +1,52 @@
+--
+-- Database create script
+-- Created by: Joey van Riemsdijk | H2
+--
 
-CREATE TABLE adjustment
-(
-    id BIGINT PRIMARY KEY NOT NULL,
+
+-- Adjustment table
+CREATE TABLE adjustment (
+    adjustment_id BIGINT PRIMARY KEY NOT NULL,
     actual_cost DOUBLE PRECISION DEFAULT 1,
     adjustment_definition BIGINT NOT NULL,
-    wmo_decision BIGINT,
-    CONSTRAINT adjustment_adjustment_definition_fkey FOREIGN KEY (adjustment_definition) REFERENCES adjustment_definitions (id),
-    CONSTRAINT adjustment_wmo_decisions_id_fk FOREIGN KEY (wmo_decision) REFERENCES wmo_decisions (id)
+    cost_subsidized DOUBLE PRECISION,
+    contractor BIGINT,
+    application BIGINT,
+    CONSTRAINT adjustment_adjustment_definition_fkey FOREIGN KEY (adjustment_definition) REFERENCES adjustment_definition (adjustment_definition_id),
+    CONSTRAINT adjustment_contractors_id_fk FOREIGN KEY (contractor) REFERENCES contractor (contractor_id),
+    CONSTRAINT adjustment_application_id_fk FOREIGN KEY (application) REFERENCES application (application_id)
 );
-CREATE UNIQUE INDEX adjustment_id_uindex ON adjustment (id);
-CREATE TABLE adjustment_condition
-(
-    adjustment BIGINT NOT NULL,
-    condition BIGINT NOT NULL,
-    CONSTRAINT adjustment_condition_adjustment_condition_pk PRIMARY KEY (adjustment, condition)
-);
-CREATE UNIQUE INDEX adjustment_condition_condition_pk ON adjustment_condition (condition);
-CREATE TABLE adjustment_definitions
-(
-    id BIGINT PRIMARY KEY NOT NULL,
+CREATE UNIQUE INDEX adjustment_id_uindex ON adjustment (adjustment_id);
+
+
+-- Adjustment definition table
+CREATE TABLE adjustment_definition (
+    adjustment_definition_id BIGINT PRIMARY KEY NOT NULL,
     name VARCHAR(254) NOT NULL,
     average_cost DOUBLE PRECISION DEFAULT 0 NOT NULL,
-    cost_margin DOUBLE PRECISION DEFAULT 0
+    cost_margin DOUBLE PRECISION DEFAULT 0,
+    creation_date TIMESTAMP,
+    version INTEGER DEFAULT 1 NOT NULL
 );
-CREATE UNIQUE INDEX adjustments_id_uindex ON adjustment_definitions (id);
-CREATE TABLE adresses
-(
-    id BIGINT PRIMARY KEY NOT NULL,
+CREATE UNIQUE INDEX adjustment_id_uindex ON adjustment_definition (adjustment_definition_id);
+
+
+-- Adjustment definition to Condition table
+CREATE TABLE adjustment_definition_to_condition (
+    adjustment BIGINT NOT NULL,
+    condition BIGINT NOT NULL,
+    start_date TIMESTAMP DEFAULT now() NOT NULL,
+    end_date TIMESTAMP,
+    CONSTRAINT adjustment_condition_adjustment_condition_pk PRIMARY KEY (adjustment, condition),
+    CONSTRAINT adjustment_condition_adjustment_fkey FOREIGN KEY (adjustment) REFERENCES adjustment_definition (adjustment_definition_id),
+    CONSTRAINT adjustment_condition_condition_fkey FOREIGN KEY (condition) REFERENCES condition (condition_id)
+);
+CREATE UNIQUE INDEX adjustment_condition_condition_pk ON adjustment_definition_to_condition (adjustment, condition);
+
+
+-- Address table
+CREATE TABLE address (
+    address_id BIGINT PRIMARY KEY NOT NULL,
     street VARCHAR,
     zipcode VARCHAR,
     number INTEGER,
@@ -34,95 +54,137 @@ CREATE TABLE adresses
     city VARCHAR,
     country VARCHAR(254)
 );
-CREATE UNIQUE INDEX adresses_id_uindex ON adresses (id);
-CREATE TABLE advice
-(
-    id BIGINT PRIMARY KEY NOT NULL,
+CREATE UNIQUE INDEX address_id_uindex ON address (address_id);
+
+
+-- Advice table
+CREATE TABLE advice (
+    advice_id BIGINT PRIMARY KEY NOT NULL,
     go_ahead BOOLEAN DEFAULT false NOT NULL,
-    applicant BIGINT,
-    CONSTRAINT advice_applicant_fkey FOREIGN KEY (applicant) REFERENCES person (bsn)
+    date_issued TIMESTAMP,
+    application BIGINT,
+    remarks VARCHAR,
+    CONSTRAINT advice_application_id_fk FOREIGN KEY (application) REFERENCES application (application_id)
 );
-CREATE UNIQUE INDEX advice_id_uindex ON advice (id);
-CREATE TABLE advice_current_condition
-(
+CREATE UNIQUE INDEX advice_id_uindex ON advice (advice_id);
+
+
+-- Advice to current condition table
+CREATE TABLE advice_to_current_condition(
     advice BIGINT NOT NULL,
     condition BIGINT NOT NULL,
     CONSTRAINT advice_current_condition_advice_condition_pk PRIMARY KEY (advice, condition),
-    CONSTRAINT advice_current_condition_advice_fkey FOREIGN KEY (advice) REFERENCES ,
-    CONSTRAINT advice_current_condition_condition_fkey FOREIGN KEY (condition) REFERENCES conditions (id)
+    CONSTRAINT advice_current_condition_advice_fkey FOREIGN KEY (advice) REFERENCES advice (advice_id),
+    CONSTRAINT advice_current_condition_condition_fkey FOREIGN KEY (condition) REFERENCES condition (condition_id)
 );
-CREATE TABLE advice_future_condition
-(
+
+
+-- Advice to future condition table
+CREATE TABLE advice_to_future_condition (
     advice BIGINT NOT NULL,
     condition BIGINT NOT NULL,
     CONSTRAINT advice_future_condition_advice_condition_pk PRIMARY KEY (advice, condition),
-    CONSTRAINT advice_future_condition_advice_fkey FOREIGN KEY (advice) REFERENCES ,
-    CONSTRAINT advice_future_condition_condition_fkey FOREIGN KEY (condition) REFERENCES conditions (id)
+    CONSTRAINT advice_future_condition_advice_fkey FOREIGN KEY (advice) REFERENCES advice (advice_id),
+    CONSTRAINT advice_future_condition_condition_fkey FOREIGN KEY (condition) REFERENCES condition (condition_id)
 );
-CREATE TABLE applicant_history
-(
-    person BIGINT NOT NULL,
-    adjustment BIGINT NOT NULL,
-    CONSTRAINT applicant_history_person_adjustment_pk PRIMARY KEY (person, adjustment),
-    CONSTRAINT applicant_history_person_fkey FOREIGN KEY (person) REFERENCES person (bsn),
-    CONSTRAINT applicant_history_adjustment_fkey FOREIGN KEY (adjustment) REFERENCES
+
+
+-- Application table
+CREATE TABLE application (
+    application_id INTEGER PRIMARY KEY NOT NULL,
+    applicant BIGINT NOT NULL,
+    date_recieved TIMESTAMP,
+    CONSTRAINT application_person_bsn_fk FOREIGN KEY (applicant) REFERENCES person (bsn)
 );
-CREATE TABLE bag
-(
-    id BIGINT PRIMARY KEY NOT NULL,
+CREATE UNIQUE INDEX application_id_uindex ON application (application_id);
+
+
+-- BAG table
+CREATE TABLE bag (
+    bag_id BIGINT PRIMARY KEY NOT NULL,
     build_year SMALLINT NOT NULL,
     building_contour VARCHAR,
     usable_surface DOUBLE PRECISION,
     use_purpose VARCHAR,
-    coordinates VARCHAR,
-    adress BIGINT,
-    CONSTRAINT bag_adress_fkey FOREIGN KEY (adress) REFERENCES adresses (id)
+    address BIGINT,
+    x DOUBLE PRECISION,
+    y DOUBLE PRECISION,
+    CONSTRAINT bag_address_fkey FOREIGN KEY (address) REFERENCES address (address_id)
 );
-CREATE UNIQUE INDEX bag_id_uindex ON bag (id);
-CREATE TABLE conditions
-(
-    id BIGINT PRIMARY KEY NOT NULL,
+CREATE UNIQUE INDEX bag_id_uindex ON bag (bag_id);
+
+
+-- Condition table
+CREATE TABLE condition (
+    condition_id BIGINT PRIMARY KEY NOT NULL,
     name VARCHAR(254),
     chronic BOOLEAN DEFAULT false NOT NULL
 );
-CREATE UNIQUE INDEX conditions_id_uindex ON conditions (id);
-CREATE TABLE housing_adjustments
-(
-    housing_situation BIGINT NOT NULL,
-    adjustment BIGINT NOT NULL,
-    CONSTRAINT housing_adjustments_housing_situation_adjustment_pk PRIMARY KEY (housing_situation, adjustment),
-    CONSTRAINT housing_adjustments_housing_situation_fkey FOREIGN KEY (housing_situation) REFERENCES housing_situation (id),
-    CONSTRAINT housing_adjustments_adjustment_fkey FOREIGN KEY (adjustment) REFERENCES
+CREATE UNIQUE INDEX condition_id_uindex ON condition (condition_id);
+
+
+-- Contractor table
+CREATE TABLE contractor (
+    contractor_id INTEGER PRIMARY KEY NOT NULL,
+    name VARCHAR(254),
+    approved BOOLEAN DEFAULT false NOT NULL,
+    cost_modifier DOUBLE PRECISION DEFAULT 0 NOT NULL,
+    date_added TIMESTAMP DEFAULT now() NOT NULL,
+    date_removed TIMESTAMP
 );
-CREATE TABLE housing_situation
-(
-    id BIGINT PRIMARY KEY NOT NULL,
-    bag BIGINT NOT NULL,
+CREATE UNIQUE INDEX contractor_id_uindex ON contractor (contractor_id);
+
+
+-- Housing situation table
+CREATE TABLE housing_situation (
+    housing_situation_id BIGINT PRIMARY KEY NOT NULL,
+    bag BIGINT,
     floor SMALLINT NOT NULL,
     elevator BOOLEAN DEFAULT false NOT NULL,
-    CONSTRAINT housing_situation_bag_fkey FOREIGN KEY (bag) REFERENCES bag (id)
+    CONSTRAINT housing_situation_bag_fkey FOREIGN KEY (bag) REFERENCES bag (bag_id)
 );
-CREATE UNIQUE INDEX housing_situation_id_uindex ON housing_situation (id);
-CREATE TABLE person
-(
+CREATE UNIQUE INDEX housing_situation_id_uindex ON housing_situation (housing_situation_id);
+
+
+-- Housing situation to Adjustment table
+CREATE TABLE housing_situation_to_adjustment (
+    housing_situation BIGINT NOT NULL,
+    adjustment BIGINT NOT NULL,
+    date_placed TIMESTAMP DEFAULT now(),
+    date_removed TIMESTAMP,
+    CONSTRAINT housing_adjustments_housing_situation_adjustment_pk PRIMARY KEY (housing_situation, adjustment),
+    CONSTRAINT housing_adjustments_housing_situation_fkey FOREIGN KEY (housing_situation) REFERENCES housing_situation (housing_situation_id) ,
+    CONSTRAINT housing_adjustments_adjustment_fkey FOREIGN KEY (adjustment) REFERENCES adjustment (adjustment_id)
+);
+
+
+-- Person table
+CREATE TABLE person (
     bsn BIGINT PRIMARY KEY NOT NULL
 );
 CREATE UNIQUE INDEX person_bsn_uindex ON person (bsn);
-CREATE TABLE residents
-(
+
+
+-- Person to Housing situation table
+CREATE TABLE person_to_housing_situation (
     person BIGINT NOT NULL,
     housing_situation BIGINT NOT NULL,
+    move_in_date TIMESTAMP,
+    move_out_date TIMESTAMP,
     CONSTRAINT residents_person_housing_situation_pk PRIMARY KEY (person, housing_situation),
     CONSTRAINT residents_person_fkey FOREIGN KEY (person) REFERENCES person (bsn),
-    CONSTRAINT residents_housing_situation_fkey FOREIGN KEY (housing_situation) REFERENCES housing_situation (id)
+    CONSTRAINT residents_housing_situation_fkey FOREIGN KEY (housing_situation) REFERENCES housing_situation (housing_situation_id)
 );
-CREATE TABLE wmo_decisions
-(
-    id INTEGER PRIMARY KEY NOT NULL,
+
+
+-- WMO decision table
+CREATE TABLE wmo_decision (
+    wmo_decision_id INTEGER PRIMARY KEY NOT NULL,
     granted BOOLEAN DEFAULT false NOT NULL,
     reason VARCHAR(1024),
     exception BOOLEAN DEFAULT false NOT NULL,
     advice BIGINT NOT NULL,
-    CONSTRAINT wmo_decisions_advice_id_fk FOREIGN KEY (advice) REFERENCES advice (id)
+    date TIMESTAMP,
+    CONSTRAINT wmo_decision_advice_id_fk FOREIGN KEY (advice) REFERENCES advice (advice_id)
 );
-CREATE UNIQUE INDEX wmo_decisions_id_uindex ON wmo_decisions (id);
+CREATE UNIQUE INDEX wmo_decision_id_uindex ON wmo_decision (wmo_decision_id);
